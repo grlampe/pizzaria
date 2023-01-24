1c37f3b20068193458a9989df229e18b398ed3a3
Gabriel Lampe <grlampe@hotmail.com>
2023-01-24 13:14:54 -0300

    Criado Migration

---
+++ prisma/migrations/20230124144456_create_user/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "fullAddress" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
