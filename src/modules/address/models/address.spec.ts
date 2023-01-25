26f23f29a95781e505a2437d7316a5400e381a7d
Gabriel Lampe <grlampe@hotmail.com>
2023-01-25 18:03:59 -0300

    Criado teste para os Setters das Entidades

--- src/modules/address/models/address.spec.ts
+++ src/modules/address/models/address.spec.ts
@@ -25,4 +25,42 @@ describe('Address', () => {
     expect(address.createdAt).toEqual(expect.any(Date));
     expect(address.userID).toEqual(expect.any(String));
   });
+
+  it('should be able to change Properties from User', () => {
+    const address = new Address(
+      {
+        userID: randomUUID(),
+        street: 'Street example',
+        city: 'City example',
+        complement: 'Complement example',
+        country: 'Country example',
+        number: 999,
+        state: 'State example',
+        zipCode: '00000-000',
+        createdAt: new Date(),
+      },
+      randomUUID(),
+    );
+
+    address.userID = randomUUID();
+    address.street = 'Another Street example';
+    address.city = 'Another City example';
+    address.complement = 'Another Complement example';
+    address.country = 'Another Country example';
+    address.number = 999;
+    address.state = 'Another State example';
+    address.zipCode = '00000-000';
+
+    expect(address).toBeTruthy();
+    expect(address.id).toEqual(expect.any(String));
+    expect(address.userID).toEqual(expect.any(String));
+    expect(address.street).toEqual('Another Street example');
+    expect(address.city).toEqual('Another City example');
+    expect(address.complement).toEqual('Another Complement example');
+    expect(address.country).toEqual('Another Country example');
+    expect(address.number).toEqual(999);
+    expect(address.state).toEqual('Another State example');
+    expect(address.zipCode).toEqual('00000-000');
+    expect(address.createdAt).toEqual(expect.any(Date));
+  });
 });
--- src/modules/user/models/user.spec.ts
+++ src/modules/user/models/user.spec.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import { User } from './user';
 
 describe('User', () => {
@@ -12,6 +13,29 @@ describe('User', () => {
     expect(user.name).toEqual('User Name Test');
     expect(user.email).toEqual('user@example.com');
     expect(user.password).toEqual('test pass');
+    expect(user.createdAt).toEqual(expect.any(Date));
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it('should be able to change Properties from User', () => {
+    const user = new User(
+      {
+        name: 'User Name Test',
+        email: 'user@example.com',
+        password: 'test pass',
+        createdAt: new Date(),
+      },
+      randomUUID(),
+    );
+
+    user.name = 'Another User Name Test';
+    user.email = 'another@example.com';
+    user.password = 'another';
+
+    expect(user).toBeTruthy();
+    expect(user.name).toEqual('Another User Name Test');
+    expect(user.email).toEqual('another@example.com');
+    expect(user.password).toEqual('another');
     expect(user.createdAt).toEqual(expect.any(Date));
     expect(user.id).toEqual(expect.any(String));
   });
